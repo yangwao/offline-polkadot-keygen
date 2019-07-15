663c7baa20bee1e584f086974db3371529d0277d
Matej Nemček <ybdaba@gmail.com>
2019-07-15 19:49:03 +0200

    experimenting with polkadot/util,polkadot/keyring

--- keyring/src/views/About.vue
+++ keyring/src/views/About.vue
@@ -1,11 +1,18 @@
 <template>
   <div class="about">
     <h1>Keyring generator for Polkadot/Substrate-like parachains accounts</h1>
-    <button @click="mainGenerate()">Generate Keyring</button>
-    <input v-model="aliceSeed">
-    <p>Created keyring pair for Alice with address:</p>
+    <input v-model="mnemonicGenerated">
+    <p>{{mnemonicGenerated}}</p>
+    <button @click="mnemonicGenerate()">Generate Mnemonic</button><br>
+    <p><button @click="validateMnemonic">Validate Mnemonic</button> {{isValidMnemonic}}</p>
+    <!-- <p>{{this.mnemonic2Seed}}</p> -->
+    <!-- <input v-model="aliceSeed"> -->
+    <!-- <button @click="mainGenerate()">Generate Keyring</button> -->
+    <button @click="mainGenerateFromMnemonic()">Generate Keyring</button>
+    <p>Created keyring pair from mnemonic address:</p>
     <p>{{keyRingPair}}</p>
     <p>{{keyRingPairType}}</p>
+    <p>{{meta}}</p>
   </div>
 </template>
 
@@ -13,6 +20,7 @@
 import { Vue, Component } from 'vue-property-decorator';
 import Keyring from '@polkadot/keyring';
 import stringToU8a from '@polkadot/util/string/toU8a';
+import { mnemonicGenerate, mnemonicToSeed, mnemonicValidate } from '@polkadot/util-crypto';
 
 @Component
 export default class About extends Vue {
@@ -20,19 +28,36 @@ export default class About extends Vue {
   public keyring: any = '';
   public keyRingPair: string = '';
   public keyRingPairType: string = '';
+  public meta: string = '';
+  public mnemonicGenerated: string = '';
+  public isValidMnemonic: boolean = false;
+  public mnemonic2Seed: any;
 
-  public mainGenerate(): void {
-    // Create account seed for Alice
-    const ALICE_SEED = this.aliceSeed.padEnd(32, ' ');
+  public mnemonicGenerate(): void {
+    this.mnemonicGenerated = mnemonicGenerate();
+    this.mnemonic2Seed = mnemonicToSeed(this.mnemonicGenerated);
+  }
 
-    // Create an instance of the Keyring
+  public validateMnemonic(): void {
+    this.isValidMnemonic = mnemonicValidate(this.mnemonicGenerated);
+  }
+
+  // public mainGenerate(): void {
+  //   // Create account seed for Alice
+  //   const ALICE_SEED = this.aliceSeed.padEnd(32, ' ');
+  //   // Create an instance of the Keyring
+  //   this.keyring = new Keyring();
+  //   // Create pair and add Alice to keyring pair dictionary (with account seed)
+  //   const pairAlice = this.keyring.addFromSeed(stringToU8a(ALICE_SEED));
+  //   this.keyRingPair = this.keyring.getPair(pairAlice.address).address;
+  //   this.keyRingPairType = this.keyring._type;
+  //   }
+
+  public mainGenerateFromMnemonic(): void {
     this.keyring = new Keyring();
-
-    // Create pair and add Alice to keyring pair dictionary (with account seed)
-    const pairAlice = this.keyring.addFromSeed(stringToU8a(ALICE_SEED));
-
+    const pairAlice = this.keyring.addFromMnemonic(this.mnemonicGenerated);
     this.keyRingPair = this.keyring.getPair(pairAlice.address).address;
     this.keyRingPairType = this.keyring._type;
-    }
   }
+}
 </script>
