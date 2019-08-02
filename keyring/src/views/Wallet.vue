0969bf9e00f251e22f360f74d1299c35f6a0d0a0
Matej Nemček <ybdaba@gmail.com>
2019-08-02 14:49:36 +0200

    adding wallet view

---
+++ keyring/src/views/Wallet.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="container is-fluid">
+    <div class="columns">
+      <div class="column is-6 is-offset-3">
+        <About/>
+        <Subkey/>
+        <Credits/>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator';
+import About from '@/components/About.vue';
+import Credits from '@/components/Credits.vue';
+import Subkey from '@/components/Subkey.vue';
+
+@Component({
+  components: {
+    About,
+    Credits,
+    Subkey,
+  },
+})
+export default class Layout extends Vue {}
+</script>
+<style scoped lang="scss">
+</style>
